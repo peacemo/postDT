c711d57303fae3dbb0bdadf09052fbc1304b8df5
Peacemo <1321500713@qq.com>
2022-04-27 15:13:55 +0800

    增加获取堆垛机id的函数

--- module_02/main.py
+++ module_02/main.py
@@ -7,57 +7,22 @@ def get_fake_data():
     return [[1,3,3.1], [2,1,2.5], [3,7,3.1], [3,8,3.4], [4,2,2.4], [5,2,1.5], [6,2,1.4], [9,5,2.3], [10,9,2.5], [11,9,2.1]]
 
 
-def get_stacker(dvcDictionary):
-    """
-    找到拆叠箱机的 id
-    dvcDictionary: 设备 id 与其名称(类型)的对应字典
-
-    return: 叠箱机的 id
-    """
-    stacker = 0
-    for key, val in dvcDictionary.items():
-        if '叠箱机' in val and '拆' not in val:
-            stacker = key
-        pass
-
-    return stacker
-
-
 def main():
     # points_info = get_data(1)
     # points_info = get_fake_data()  # 获取边数据
     points_info, dvcDictionary = GraphGenerator.getEdges()
     graph = GraphGenerator.gen_graph(points_info)  # 通过边数据生成有向图邻接矩阵
-    stacker = get_stacker(dvcDictionary)  # 获取拆叠箱机的位置
+    stacker = Tools.get_stacker(dvcDictionary)  # 获取拆叠箱机的位置
     # show_info(graph)
 
     level, graph.nodes[0] = BFS.AMG(graph, stacker)  # 对图进行广搜，获取层次
     print(level)
     print(graph.nodes[0])
     
-    #################################################
-    # bias = abs( min(level) ) + 1
-    # tempDict = {}
+    points_info = Tools.stdOutput(level, graph.nodes[0], points_info)
 
-    # for i in range(len(level)):
-    #     tempDict.update({graph.nodes[0][i] : level[i]})
-
-    # for item in points_info:
-    #     try:
-    #         if tempDict[item[0]] >= 0:
-    #             item.insert(0, tempDict[item[0]] + 1 + bias )
-    #         else:
-    #             item.insert(0, tempDict[item[0]] + bias)
-    #         pass
-    #     except:
-    #         print("入库产线不包含此点")
-    
-    Tools.stdOutput(level, graph.nodes[0], points_info)
-    
     print(points_info)
-    #################################################
 
-    # TODO 去除不可到达的点位
     return points_info
 
 if __name__ == "__main__":
--- module_02/utils/tools.py
+++ module_02/utils/tools.py
@@ -21,15 +21,23 @@ class Tools:
 
 
     @staticmethod
-    def stdOutput(level: list, nodes: list, pointsInfo):
+    def stdOutput(level: list, nodes: list, pointsInfo: list) -> list:
+        """
+        标准化输出: 将输出的边按层级和时间消耗排序, 层级升序, 时间降序
+        level: 层级信息
+        nodes: 顶点信息
+        pointsInfo: 边信息
+        return: 带有层级信息且排好序的边信息用作输出
+        """
 
-        bias = abs( min(level) ) + 1
-        tempDict = {}
+        bias = abs( min(level) ) + 1  # 为所有的层级添加一个权重
+        tempDict = {}  # 用字典记录下每一个点的层级
 
         for i in range(len(level)):
             tempDict.update({nodes[i] : level[i]})
 
         for item in pointsInfo:
+            # 对于边表中的每一条边,插入其起点的层级参数
             try:
                 if tempDict[item[0]] >= 0:
                     item.insert(0, tempDict[item[0]] + 1 + bias )
@@ -39,5 +47,25 @@ class Tools:
             except:
                 print("入库产线不包含此点")
         
+        pointsInfo = sorted(pointsInfo, key=lambda x: (x[0], -x[-1]))  # 按照pointsInfo中的第 1 个和最后一个值排序,第一个字段升序,最后一个字段降序
+        pointsInfo = [points for points in pointsInfo if points[0] != float('inf')]  # 去除不连通的边
+        # print(pointsInfo)
+
         return pointsInfo
         pass
+
+    @staticmethod
+    def get_stacker(dvcDictionary):
+        """
+        找到拆叠箱机的 id
+        dvcDictionary: 设备 id 与其名称(类型)的对应字典
+
+        return: 叠箱机的 id
+        """
+        stacker = 0
+        for key, val in dvcDictionary.items():
+            if '叠箱机' in val and '拆' not in val:
+                stacker = key
+            pass
+
+        return stacker
